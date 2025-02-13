2aee16639deba18e8a3795d953f0a03d5baf96f3
Piyush <piyushagarwal1408@gmail.com>
2025-02-13 13:41:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+
+
+
+let setCompanyName = $(".container");
+
 const url = 'https://linkedin-data-api.p.rapidapi.com/get-company-insights?username=amazon';
 const options = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': 'Enter Api key here',
+		'x-rapidapi-key': 'enter your api here',
 		'x-rapidapi-host': 'linkedin-data-api.p.rapidapi.com'
 	}
 };
@@ -12,10 +17,13 @@ async function fetchCompanyInsights() {
 		const response = await fetch(url, options);
 		const result = await response.json(); 
 		console.log(result.data.company.name);
+        let companyName = result.data.company.name;
+        $(".container").text(companyName); 
 	} catch (error) {
 		console.error(error);
 	}
 }
+
 
 // Call the async function
 fetchCompanyInsights();
